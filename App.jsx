0b5ec09112b0de8782d3123918ce1cf89f026851
Parinita Ray <rayparinita75@gmail.com>
2025-06-05 11:14:21 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FacultyDashboard from "./FacultyDashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <FacultyDashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ FacultyDashboard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./FacultyDashboard.css";
+
+const FacultyDashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <div className="profile-card">
+        <div className="avatar"></div>
+        <div className="name">NAME / USERNAME</div>
+        <div className="info-box">
+          <p>Email</p>
+          <p>Department</p>
+          <p>Qualification</p>
+        </div>
+      </div>
+      <div className="actions">
+        <button className="action-button">Create class</button>
+        <button className="action-button">add works</button>
+      </div>
+    </div>
+  );
+};
+
+export default FacultyDashboard;
